a4ab7ccb1d76525f2fb903b3502dcc583e39146a
GordonDamsel0220 <dbitsgrinder@gmail.com>
2025-01-31 08:28:53 +0700

    31/1/25

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Wait for the DOM to finish loading before initializing Swiper
 document.addEventListener('DOMContentLoaded', () => {
     const swiper = new Swiper(".carousel-container", {
-      effect: "slide",
+      effect: "fade",
       speed: 1300,
       autoplay: {
         delay: 4000
